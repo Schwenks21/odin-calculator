e74957c2cef4d8f477a82136dd8caacd692cabfb
Matt Schwenkner <mjschwenkner@gmail.com>
2022-02-14 10:33:43 -0600

    feat: added script for basic math functions

---
+++ script.js
@@ -0,0 +1,19 @@
+function add (x, y) {
+    return x + y;
+}
+
+function subtract (x, y) {
+    return x - y;
+}
+
+function multiply (x, y) {
+    return x * y;
+}
+
+function divide (x, y) {
+    return x/y;
+}
+
+function operate (operator, x, y) {
+    
+}
